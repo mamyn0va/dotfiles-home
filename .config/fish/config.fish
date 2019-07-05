65ce29dc25642ef1f4374524cd52a601f50fe960
biros <boris.jamot@protonmail.com>
2019-07-05 23:39:44 +0200

    update conf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,6 @@
 set -gx EDITOR nvim
 set -gx VISUAL nvim
+set -gx SHELL /usr/bin/bash
 set -gx PROJECT_PATHS ~/projects
 set -gx PATH /home/biros/.gem/ruby/2.6.0/bin $PATH
 set -g theme_powerline_fonts yes
