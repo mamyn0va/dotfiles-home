a3f20792c41b52bf5c4d41afbc9dbef61cb74859
biros <boris.jamot@protonmail.com>
2020-12-11 22:22:02 +0100

    Add backlight module to polybar

--- .config/fish/functions/cat.fish
+++ .config/fish/functions/cat.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function cat --description 'alias cat=bat --paging=never --style=plain'
-	bat --paging=never --style=plain $argv;
+function cat --wraps='bat --paging=never --style=plain' --description 'alias cat=bat --paging=never --style=plain'
+  bat --paging=never --style=plain $argv;
 end
---
+++ .config/fish/functions/df.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function df --wraps=duf --description 'alias df=duf'
+  duf  $argv;
+end
